39a65ca225541da4ef44bd4c2c8faa95e5c01d0f
Milan Boers <mail@milanboers.nl>
2021-12-13 15:44:20 +0100

    Day 13 optimized

--- app/13/part1.hs
+++ app/13/part1.hs
@@ -1,5 +1,6 @@
 import Data.List.Split (splitOn)
 import Data.List (nub)
+import Data.Set as Set (Set, map, fromList)
 
 type Pos = (Int, Int)
 type Axis = String
@@ -10,10 +11,10 @@ foldDot ("x",n) dot@(x,y) = if (x-n) > 0 then (-x+2*n, y) else dot
 foldDot ("y",n) dot@(x,y) = if (y-n) > 0 then (x, -y+2*n) else dot
 foldDot _ _ = error "Wrong fold axis"
 
-foldDots :: Fold -> [Pos] -> [Pos]
-foldDots fold = nub . map (foldDot fold)
+foldDots :: Fold -> Set Pos -> Set Pos
+foldDots fold = Set.map (foldDot fold)
 
-findAnswer :: [Fold] -> [Pos] -> Int
+findAnswer :: [Fold] -> Set Pos -> Int
 findAnswer folds = length . foldDots (head folds)
 
 main :: IO ()
@@ -21,6 +22,6 @@ main = do
   contents <- getContents
   let input = lines contents
   let [dots', folds'] = splitOn [""] input
-  let dots = map ((\[x, y] -> (read x, read y)) . splitOn ",") dots'
-  let folds = map ((\[axis, n] -> (axis, read n)) . (splitOn "=" . drop 11)) folds'
-  print $ findAnswer folds dots
+  let dots = Prelude.map ((\[x, y] -> (read x, read y)) . splitOn ",") dots'
+  let folds = Prelude.map ((\[axis, n] -> (axis, read n)) . (splitOn "=" . drop 11)) folds'
+  print $ findAnswer folds (fromList dots)
--- app/13/part2.hs
+++ app/13/part2.hs
@@ -1,5 +1,6 @@
 import Data.List.Split (splitOn)
 import Data.List (nub, intercalate)
+import Data.Set as Set (Set, fromList, map, member)
 
 type Pos = (Int, Int)
 type Axis = String
@@ -10,20 +11,20 @@ foldDot ("x",n) dot@(x,y) = if (x-n) > 0 then (-x+2*n, y) else dot
 foldDot ("y",n) dot@(x,y) = if (y-n) > 0 then (x, -y+2*n) else dot
 foldDot _ _ = error "Wrong fold axis"
 
-foldDots :: Fold -> [Pos] -> [Pos]
-foldDots fold = nub . map (foldDot fold)
+foldDots :: Fold -> Set Pos -> Set Pos
+foldDots fold = Set.map (foldDot fold)
 
-foldAll :: [Fold] -> [Pos] -> [Pos]
+foldAll :: [Fold] -> Set Pos -> Set Pos
 foldAll folds dots = foldl (flip foldDots) dots folds
 
-dotsToOutput :: [Pos] -> String
+dotsToOutput :: Set Pos -> String
 dotsToOutput dots = intercalate "\n" lines
   where
-    maxX = maximum $ map fst dots
-    maxY = maximum $ map snd dots
-    lines = [[if (x, y) `elem` dots then '%' else ' ' | x <- [0..maxX]] | y <- [0..maxY]]
+    maxX = maximum $ Set.map fst dots
+    maxY = maximum $ Set.map snd dots
+    lines = [[if (x, y) `member` dots then '%' else ' ' | x <- [0..maxX]] | y <- [0..maxY]]
 
-findAnswer :: [Fold] -> [Pos] -> String
+findAnswer :: [Fold] -> Set Pos -> String
 findAnswer folds dots = dotsToOutput $ foldAll folds dots
 
 main :: IO ()
@@ -31,6 +32,6 @@ main = do
   contents <- getContents
   let input = lines contents
   let [dots', folds'] = splitOn [""] input
-  let dots = map ((\[x, y] -> (read x, read y)) . splitOn ",") dots'
-  let folds = map ((\[axis, n] -> (axis, read n)) . (splitOn "=" . drop 11)) folds'
-  putStrLn $ findAnswer folds dots
+  let dots = Prelude.map ((\[x, y] -> (read x, read y)) . splitOn ",") dots'
+  let folds = Prelude.map ((\[axis, n] -> (axis, read n)) . (splitOn "=" . drop 11)) folds'
+  putStrLn $ findAnswer folds (fromList dots)
